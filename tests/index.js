e9c073e70141b426f2b07730158b3a1b03983b92
Alex Forbes-Reed <alexreed@outlook.com>
2016-05-07 00:03:23 +0100

    lol

--- tests/index.js
+++ tests/index.js
@@ -16,29 +16,31 @@ if (fbToken === undefined || fbId === undefined) {
 	// Create Tinder Client	
 	const client = new TinderClient(token);
 
-	const ladies = JSON.parse(await fs.readFile('.recs.json'));
-	
-	// for (let i = 0; i < 50; i++) {
-	// 	const recs = (await client.RecomendationsClient.get()).results;
-	// 	for (const rec of recs) {
-	// 		if (ladies[rec._id] === void 0)
-	// 			ladies[rec._id] = rec;
-	// 	}
-	// }
+	for (let i = 0; i < 20; i++) {
+		const ladies = JSON.parse(await fs.readFile('.recs.json.new'));
 
-	let index = 0;
-	for (const ladyId in ladies) {
-		const lady = ladies[ladyId];
-		if (lady.matched === true) continue;
-		let resp = void 0;
-		try {
-			resp = await client.LikesClient.get(lady._id, lady.photos[0].id);
-		} catch (error) { console.log(error); }
-		await new Promise(resolve => { setTimeout(resolve, 100); });
-		console.log(`[${index} - ${JSON.stringify(resp)}]`);
-		ladies[ladyId].matched = true;
-		index++;
+		const recs = (await client.RecomendationsClient.get()).results;
+		for (const rec of recs) {
+			if (ladies[rec._id] === void 0) {
+				ladies[rec._id] = rec;
+				console.log(`Added new entry: ${rec._id}`);
+			}
+		}
+
+		let index = 0;
+		for (const ladyId in ladies) {
+			const lady = ladies[ladyId];
+			if (lady.matched === true) continue;
+			let resp = void 0;
+			try {
+				resp = await client.LikesClient.get(lady._id, lady.photos[0].id);
+			} catch (error) { console.log(error); }
+			console.log(`[${index} - ${JSON.stringify(resp)}] - ${ladyId}`);
+			ladies[ladyId].matched = true;
+			index++;
+		}
+
+		await fs.writeFile('.recs.json.new', JSON.stringify(ladies, null, '\t'));
 	}
 
-	await fs.writeFile('.recs.json', JSON.stringify(ladies, null, '\t'));
 })();
